24aac1cdf038755fcd645d877a8cf666050d7fca
Nora Grundberg <nora94grundberg@gmail.com>
2024-09-11 20:50:48 +0200

    lägg till projektbeskrivningar

--- projectsData.js
+++ projectsData.js
@@ -4,24 +4,34 @@ export const projectsData = [
       technologies: ["HTML", "CSS", "JavaScript"],
       description:
          "This is a clone of the Spotify web application with custom styling and API integration.",
+
       reflections:
          "During this project, I learned about responsive design and API handling.",
       gainedKnowledge:
          "Improved understanding of CSS Flexbox, Grid, and async JavaScript.",
+      imgSrc: "images/spotifycloneimage.png",
    },
    {
-      name: "Project 2",
-      technologies: ["React", "Node.js", "MongoDB"],
-      description: "beskriv projektet",
-      reflections: "reflektera över projektets gång",
-      gainedKnowledge: "vad lärde jag mig?",
+      name: "STIG wear",
+      technologies: ["HTML", "CSS", "Javascript", "Figma"],
+      description:
+         "Projektet är en webbshop som är designad utifrån de bildmaterialet som gick att tillgå. Anpassad för mobil, surfplatta och webb. Några vanliga funktioner för webbshopsstandar har gjort. Där användarvänlighet och branddeign har legat i fokus. ",
+      reflections:
+         "Det finns väldigt många fördelar med att göra ett gediget förarbete designmässigt vilket jag tar med mig till nästa projekt. Om projektet förväntas fungera för mobil, surfplatta och webb bör man utgå ifrån principen mobil-first. För en större webb-shop hade man behövt anpassa den efter skalbarhet på ett helt annat sätt, men syftet med denna var främst att bekanta mig med nya verktyg och öva min problemlösningsförmåga",
+      gainedKnowledge:
+         "Projektet var ett ypperlig tillfälle att öva på att göra responsiv design, bekanda mig med Figma och öva på Javascript och CSS.",
+      imgSrc: "images/stigwearimg.png",
    },
    {
-      name: "Project 3",
-      technologies: ["React", "Node.js", "MongoDB"],
-      description: "beskriv projektet",
-      reflections: "reflektera över projektets gång",
-      gainedKnowledge: "vad lärde jag mig?",
+      name: "Portfolio",
+      technologies: ["HTML", "Sass", "Javascript", "Figma", "Git"],
+      description:
+         "Min portfolio, är skapad för att spegla mig i design, kod och funktionalitet. Den är anpassad för webb och designad för att vara skalbar så att jag på sikt kan addera fler projekt. I detta projekt implementerade jag Sass för att strukturera min CSS bättre.  ",
+      reflections:
+         "Det underlättade att designa upp projektet innan jag kodade det, då gick jag en bra översikt och målmedvetenhet över funktioner och designelement jag villa addera vilket också gav utrymme att vara med kreativ och nytänkade i min js. Att använda ett stilarkspråk underlättade organiationen av min css avsevärt och förhoppningarna är att skalbarheten och underhållandet av min kod på sikt kommer förenklas. Här har jag även använt mig av Git för att vänja mig vid fördelar och arbetsstruktur som kan vara bra inför framtiden. ",
+      gainedKnowledge:
+         "användningen av sass, djupare förståelse för javascript och vikten av att designa en bra grund innan man börjar koda. Bekantat mig med Git och hur man använder det för commits under projektets gång",
+      imgSrc: "images/portfolio.png",
    },
    {
       name: "Project 4",
@@ -29,6 +39,7 @@ export const projectsData = [
       description: "beskriv projektet",
       reflections: "reflektera över projektets gång",
       gainedKnowledge: "vad lärde jag mig?",
+      imgSrc: "",
    },
    //    addera projekt här
 ];
