c1b3e6b687bbbed0997924d60e36336cfbab7271
Nora Grundberg <nora94grundberg@gmail.com>
2024-09-11 09:34:21 +0200

    addera objectarray till project content

---
+++ projectsData.js
@@ -0,0 +1,34 @@
+export const projectsData = [
+   {
+      name: "Spotify Clone",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      description:
+         "This is a clone of the Spotify web application with custom styling and API integration.",
+      reflections:
+         "During this project, I learned about responsive design and API handling.",
+      gainedKnowledge:
+         "Improved understanding of CSS Flexbox, Grid, and async JavaScript.",
+   },
+   {
+      name: "Project 2",
+      technologies: ["React", "Node.js", "MongoDB"],
+      description: "beskriv projektet",
+      reflections: "reflektera över projektets gång",
+      gainedKnowledge: "vad lärde jag mig?",
+   },
+   {
+      name: "Project 3",
+      technologies: ["React", "Node.js", "MongoDB"],
+      description: "beskriv projektet",
+      reflections: "reflektera över projektets gång",
+      gainedKnowledge: "vad lärde jag mig?",
+   },
+   {
+      name: "Project 4",
+      technologies: ["React", "Node.js", "MongoDB"],
+      description: "beskriv projektet",
+      reflections: "reflektera över projektets gång",
+      gainedKnowledge: "vad lärde jag mig?",
+   },
+   //    addera projekt här
+];
