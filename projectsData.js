1c19aed9cc59a9967b75b71a78ec9d0e2fa09746
noraGrundberg <nora94grundberg@gmail.com>
2024-09-16 17:54:43 +0200

    ändra text kontent

--- projectsData.js
+++ projectsData.js
@@ -15,9 +15,9 @@ export const projectsData = [
       name: "STIG wear",
       technologies: ["HTML", "CSS", "Javascript", "Figma"],
       description:
-         "Projektet är en webbshop som är designad utifrån de bildmaterialet som gick att tillgå. Anpassad för mobil, surfplatta och webb. Några vanliga funktioner för webbshopsstandar har gjort. Där användarvänlighet och branddeign har legat i fokus. ",
+         "Projektet är en webbshop som är designad utifrån de bildmaterialet som gick att tillgå. Anpassad för mobil, surfplatta och webb. Några vanliga funktioner för webbshopsstandar har gjort. Där användarvänlighet och brand/design har legat i fokus. Det primära syftet med projektet var att hiva mig ut för att få en djupare förståelse för Css och javascript, mycket genom trail and error principen. Jag hoppades på att vinna insikter längst projektets gång som är till nytta för framtida projekt. ",
       reflections:
-         "Det finns väldigt många fördelar med att göra ett gediget förarbete designmässigt vilket jag tar med mig till nästa projekt. Om projektet förväntas fungera för mobil, surfplatta och webb bör man utgå ifrån principen mobil-first. För en större webb-shop hade man behövt anpassa den efter skalbarhet på ett helt annat sätt, men syftet med denna var främst att bekanta mig med nya verktyg och öva min problemlösningsförmåga",
+         "Att testa och misslykas och testa igen ökade förståelsen för hur jag på bästa sätt applicerar olika stilsättningar för responsiv design och vad som fungerar och inte fungerar. Det finns väldigt många fördelar med att göra ett gediget förarbete designmässigt vilket jag tar med mig till nästa projekt. Om projektet förväntas fungera för mobil, surfplatta och webb bör man utgå ifrån principen mobil-first. För en större webb-shop hade man behövt anpassa den efter skalbarhet på ett helt annat sätt",
       gainedKnowledge:
          "Projektet var ett ypperlig tillfälle att öva på att göra responsiv design, bekanda mig med Figma och öva på Javascript och CSS.",
       imgSrc: "images/stigwearimg.png",
@@ -26,9 +26,9 @@ export const projectsData = [
       name: "Portfolio",
       technologies: ["HTML", "Sass", "Javascript", "Figma", "Git"],
       description:
-         "Min portfolio, är skapad för att spegla mig i design, kod och funktionalitet. Den är anpassad för webb och designad för att vara skalbar så att jag på sikt kan addera fler projekt. I detta projekt implementerade jag Sass för att strukturera min CSS bättre.  ",
+         "Min portfolio, är skapad för att spegla mig i design, kod och funktionalitet. Den är anpassad för webb och designad för att vara skalbar så att jag på sikt kan addera fler projekt och för att den ska vara lätt att underhålla. I detta projekt implementerade jag Sass för att strukturera min CSS bättre.  ",
       reflections:
-         "Det underlättade att designa upp projektet innan jag kodade det, då gick jag en bra översikt och målmedvetenhet över funktioner och designelement jag villa addera vilket också gav utrymme att vara med kreativ och nytänkade i min js. Att använda ett stilarkspråk underlättade organiationen av min css avsevärt och förhoppningarna är att skalbarheten och underhållandet av min kod på sikt kommer förenklas. Här har jag även använt mig av Git för att vänja mig vid fördelar och arbetsstruktur som kan vara bra inför framtiden. ",
+         "Det underlättade att designa upp projektet innan jag kodade det, då gick jag en bra översikt och målmedvetenhet över funktioner och designelement jag villa addera vilket också gav utrymme att vara med kreativ och nytänkade i min js men även utvecklat förståelse för debugging. Att använda ett stilarkspråk underlättade organiationen av min css avsevärt och förhoppningarna är att skalbarheten och underhållandet av min kod på sikt kommer förenklas. Här har jag även använt mig av Git för att vänja mig vid fördelar och arbetsstruktur som kan vara bra inför framtiden. ",
       gainedKnowledge:
          "användningen av sass, djupare förståelse för javascript och vikten av att designa en bra grund innan man börjar koda. Bekantat mig med Git och hur man använder det för commits under projektets gång",
       imgSrc: "images/portfolio.png",
