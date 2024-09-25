339987c6a97b9cfd433d5ff51ba58cd7744cf919
noraGrundberg <nora94grundberg@gmail.com>
2024-09-25 17:01:21 +0200

    addera projektdata för ny projektslide

---
+++ otherProjectsData.js
@@ -0,0 +1,10 @@
+export const otherProjectsData = [
+   {
+      name: "Spotify Clone",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      description:
+         "This is a clone of the Spotify web application with custom styling and API integration.",
+      imgSrc: "images/spotifycloneimage.png",
+      githubLink: "",
+   },
+];
